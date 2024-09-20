9ac60f67f30a3cf40a52b90372f0676979e4cec6
ethan <ethan7palmer@gmail.com>
2024-09-20 13:57:43 -0600

    edited player script some more

--- test-game/player.gd
+++ test-game/player.gd
@@ -1,10 +1,11 @@
-extends Sprite2D
+extends Node2D
+class_name Player
 
-@onready var animated_sprite = $PlayerAnimation
+@onready var animated_sprite = $Character/PlayerAnimation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
